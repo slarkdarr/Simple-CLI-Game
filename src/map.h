12fd359fe9b0f635b81ae581356e081d0bd1e451
Govindabhakta <igovindabhakta@gmail.com>
2020-10-26 17:25:43 +0700

    Map.h updated with new mapEntry spec

--- src/map.h
+++ src/map.h
@@ -5,19 +5,37 @@
 
 #include "matriks.h"
 #include "boolean.h"
+#include "point.h"
 
-typedef int keytype;
+typedef char keytype;
 typedef int infotype;
 typedef struct {
-                keytype key;
-                infotype value;
+                keytype type; 
+                infotype id; 
                 } mapEntry;
 
 typedef struct {
-                mapEntry T[MaxEl-1];
+                mapEntry T[MaxEl-1][MaxEl-1];
                 int NbElmt;
+                POINT P;
                 } Map;
 
-boolean IsWahana(MATRIKS MAP, int row, int col);
+/* KONSTRUKTOR */
+MakeEmptyMap(Map *M);
+LoadMap(Map *M);
+
+/* GETTER SETTER */
+
+
+/* PROSEDUR */
+// Menggerakkan Point P sesuai command W
+Move(Map *M, char W);
+
+// SystemPrint matriks Map M
+DrawMap(Map M);
+
+/* FUNGSI */
+// Mengembalikan id dari object dengan tipe O terdekat dengan P
+GetObject(char O);
 
 #endif
